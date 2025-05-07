db2d2e15b97604184280a55a03ad8e3757330737
raymond-chao <raymond_chao45@hotmail.com>
2025-05-07 21:27:23 +0200

    changes css and jsx

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,15 +1,13 @@
-import Project1 from './components/project1';
-import Project2 from './components/project2';
-import Project3 from './components/project3';
+import Project1 from "../components/project1.jsx";
+import Project2 from "../components/project2.jsx";
+import Project3 from "../components/project3.jsx";
 
 function Portfolio() {
-  return (
-    <div>
+  return <div>
       <Project1 />
       <Project2 />
       <Project3 />
     </div>
-  );
 }
 
 export default Portfolio;
