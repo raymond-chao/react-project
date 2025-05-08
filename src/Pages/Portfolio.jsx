4b326f7cf323886d76cc90e9dd367e36f7f75992
raymond-chao <raymond_chao45@hotmail.com>
2025-05-08 16:49:48 +0200

    changes

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -18,7 +18,6 @@ function Portfolio() {
       <h1 className="text-3xl font-bold mb-6">My Projects</h1>
 
       <div className="w-full max-w-4xl space-y-6">
-        {/* Pass the handleProjectClick function to ProjectsList */}
         <ProjectsList onProjectClick={handleProjectClick} />
       </div>
 
